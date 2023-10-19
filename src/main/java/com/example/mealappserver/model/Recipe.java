f62317ca4da08deb6ad5d81330564433bf61bd4c
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 14:58:03 -0400

    add getters, setters and toString method to Recipe  model

--- src/main/java/com/example/mealappserver/model/Recipe.java
+++ src/main/java/com/example/mealappserver/model/Recipe.java
@@ -24,5 +24,63 @@ public class Recipe {
         this.sourceUrl = sourceUrl;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ingredients='" + ingredients + '\'' +
+                ", instructions='" + instructions + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", sourceUrl='" + sourceUrl + '\'' +
+                '}';
+    }
 }
