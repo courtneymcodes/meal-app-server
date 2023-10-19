1f9fbafae86524a7d9382d5e2dd91c175dfd4271
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 15:59:27 -0400

    create Cart model, add fields

---
+++ src/main/java/com/example/mealappserver/model/Cart.java
@@ -0,0 +1,12 @@
+package com.example.mealappserver.model;
+
+import java.util.List;
+
+public class Cart {
+
+    private String id;
+
+    private String name;
+
+    private List<Ingredient> items;
+}
