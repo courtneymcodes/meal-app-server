4fcbad3bb4be1d4be0e98ae79ed6a9dbde7af5f8
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 14:32:40 -0400

    mark User class as Entity, name table users

--- src/main/java/com/example/mealappserver/model/User.java
+++ src/main/java/com/example/mealappserver/model/User.java
@@ -1,5 +1,10 @@
 package com.example.mealappserver.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
     private Long id;
 
