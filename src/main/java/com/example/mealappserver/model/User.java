b3b4978149de2f057ca4b6f382d2fa194e281970
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 14:11:18 -0400

    create User model, add feilds

---
+++ src/main/java/com/example/mealappserver/model/User.java
@@ -0,0 +1,12 @@
+package com.example.mealappserver.model;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String emailAddress;
+
+    private String password;
+
+}
