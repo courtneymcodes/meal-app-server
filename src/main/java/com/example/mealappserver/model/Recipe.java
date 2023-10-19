26dceb99de2673222c1b8c1bbb478e11f2ffbf22
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 14:52:20 -0400

    add constructors to Recipe model

--- src/main/java/com/example/mealappserver/model/Recipe.java
+++ src/main/java/com/example/mealappserver/model/Recipe.java
@@ -12,4 +12,17 @@ public class Recipe {
     private String imageUrl;
 
     private String sourceUrl;
+
+    public Recipe() {
+    }
+
+    public Recipe(String name, String ingredients, String instructions, String imageUrl, String sourceUrl) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.imageUrl = imageUrl;
+        this.sourceUrl = sourceUrl;
+    }
+
+
 }
