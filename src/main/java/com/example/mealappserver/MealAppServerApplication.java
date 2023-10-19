332fb104daee202ae911470f27d80f212e274fd5
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 13:57:00 -0400

    initial commit

---
+++ src/main/java/com/example/mealappserver/MealAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mealappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mealappserver/MealAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
