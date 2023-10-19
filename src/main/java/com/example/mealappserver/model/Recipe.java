97e990e3ddf89b0055bf04e1681c684ca7b161cd
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-19 14:50:58 -0400

    create Recipe model, add fields

---
+++ src/main/java/com/example/mealappserver/model/Recipe.java
@@ -0,0 +1,15 @@
+package com.example.mealappserver.model;
+
+public class Recipe {
+    private Long id;
+
+    private String name;
+
+    private String ingredients;
+
+    private String instructions;
+
+    private String imageUrl;
+
+    private String sourceUrl;
+}
