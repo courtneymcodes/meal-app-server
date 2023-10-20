8c842071f2a51d345bcd08f18d669e655c89a45b
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 19:44:23 -0400

    implement Security Filter Chain method

--- src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
+++ src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
@@ -2,8 +2,22 @@ package com.example.mealappserver.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/auth/users","/auth/users/login/","/auth/users/register/").permitAll()  //allow access to this list of urls
+                .antMatchers("/h2-console/**").permitAll() // allow access to any url starting with /h2-console/
+                .anyRequest().authenticated()  //must authenticate any request
+                .and().sessionManagement()  //must manage user session
+                .and().csrf().disable() //disable Cross-Site Request Forgery protection
+                .headers().frameOptions().disable();  //for rendering h2 database
+        return http.build();
+    }
 }
