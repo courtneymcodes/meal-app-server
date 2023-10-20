dbc1742af0dac4e1860d14fd01e40a867490d2b6
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 15:04:34 -0400

    add annotations to UserControllerTestsDefs, set tests to run on random port

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -1,7 +1,11 @@
 package definitions;
 
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTestDefs {
     @When("A new user registers with email and password")
     public void aNewUserRegistersWithEmailAndPassword() {
