cb169ae1f4aa38d3409a5445f7a6d26cd522f9fa
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 14:48:38 -0400

    add first test to UserControllerTestDefs

---
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -0,0 +1,10 @@
+package definitions;
+
+import io.cucumber.java.en.When;
+
+public class UserControllerTestDefs {
+    @When("A new user registers with email and password")
+    public void aNewUserRegistersWithEmailAndPassword() {
+
+    }
+}
