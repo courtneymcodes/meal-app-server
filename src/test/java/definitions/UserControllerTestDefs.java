e0101f7b2bb23c4ef06f459b844489fc03788083
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 15:10:09 -0400

    specify main application class, add logger

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -1,12 +1,18 @@
 package definitions;
 
+import com.example.mealappserver.MealAppServerApplication;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Logger;
+
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MealAppServerApplication.class)
 public class UserControllerTestDefs {
+
+    private final Logger logger = (Logger) LoggerFactory.getLogger(UserControllerTestDefs.class);
     @When("A new user registers with email and password")
     public void aNewUserRegistersWithEmailAndPassword() {
 
