7332f1998690cc927f6afb526003b955d7f8cf72
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 19:49:01 -0400

    add javadoc to filter chain method

--- src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
+++ src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
@@ -10,7 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
 
-
+    /**
+     * Every request from server must go through SecurityFilterChain which determines whether access is allowed.
+     * @param http incoming http request
+     * @return configured SecurityFilterChain
+     * @throws Exception if error occurs
+     */
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests().antMatchers("/auth/users","/auth/users/login/","/auth/users/register/").permitAll()  //allow access to this list of urls
                 .antMatchers("/h2-console/**").permitAll() // allow access to any url starting with /h2-console/
