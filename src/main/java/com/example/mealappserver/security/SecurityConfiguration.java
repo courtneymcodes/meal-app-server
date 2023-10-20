b119e120f3a594aa544079a3a2d0d3578cc3d11d
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 19:26:16 -0400

    add annotations to SecurityConfiguration class, enable global method security

--- src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
+++ src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
@@ -1,4 +1,9 @@
 package com.example.mealappserver.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 }
