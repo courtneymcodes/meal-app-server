36d64c1fc1fee84dafbcd17cb17c09a2567d5163
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 22:31:43 -0400

    implement loadUserByUsername method in MyUserDetailsService

---
+++ src/main/java/com/example/mealappserver/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.mealappserver.security;
+
+import com.example.mealappserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public MyUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        User user = userService.getUserByEmailAddress(emailAddress);
+        return new MyUserDetails(user);
+    }
+}
