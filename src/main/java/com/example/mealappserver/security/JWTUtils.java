1a6d23a6c0160d4f82b49495b7b44717d71eedd5
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-21 09:16:54 -0400

    create method to validate jwt token

--- src/main/java/com/example/mealappserver/security/JWTUtils.java
+++ src/main/java/com/example/mealappserver/security/JWTUtils.java
@@ -1,10 +1,10 @@
 package com.example.mealappserver.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //
@@ -39,4 +39,29 @@ public class JWTUtils {
     public String getUsernameFromJwtToken(String token) {
         return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).getBody().getSubject();
     }
+
+    /**
+     * Validates the JWT token
+     * @param token JWT being validated
+     * @return true if JWT is valid. Returns false if not valid or exception occurs
+     * throws exception if token invalid
+     */
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            logger.log(Level.SEVERE, "Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.log(Level.SEVERE, "Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.SEVERE, "JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
 }
