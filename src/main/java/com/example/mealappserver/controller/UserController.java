a27bfc06de2f4a97bbe884cb538853af3e7d852b
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-21 11:08:34 -0400

    create UserController, add RestController annotaion, inject UserService

---
+++ src/main/java/com/example/mealappserver/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.mealappserver.controller;
+
+import com.example.mealappserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
