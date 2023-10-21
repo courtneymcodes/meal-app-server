a0fd09791340426b09c2347ea3a00cf707f21421
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 21:26:24 -0400

    implement createUser method

--- src/main/java/com/example/mealappserver/service/UserService.java
+++ src/main/java/com/example/mealappserver/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.mealappserver.service;
 
+import com.example.mealappserver.exception.InformationExistsException;
+import com.example.mealappserver.model.User;
 import com.example.mealappserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -21,5 +23,18 @@ public class UserService {
     public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    //create method to create a user that takes in a userObject
+    //make sure they do not already exist in the database
+    //if not, return the saved the user to the database (encode password)
+    //if so throw InformationExistsException
+    public User createUser(User userObject) {
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {  //if email doesn't already exist in the database
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));  //encode the password and save to userObject
+            return userRepository.save(userObject);  //save userObject with encoded password to database
+        } else {  //if email address already exists in database, throw an exception
+            throw new InformationExistsException("Email address " + userObject.getEmailAddress() + " already exists");
+        }
     }
 }
