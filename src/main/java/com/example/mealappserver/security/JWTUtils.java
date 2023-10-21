7d96e7b3b81c0637c90a1daf91d836a400ae56da
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-21 08:52:37 -0400

    implement method in JWTUtils to generate token when user logs in

--- src/main/java/com/example/mealappserver/security/JWTUtils.java
+++ src/main/java/com/example/mealappserver/security/JWTUtils.java
@@ -1,7 +1,10 @@
 package com.example.mealappserver.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 //
@@ -14,5 +17,17 @@ public class JWTUtils {
     @Value("${jwt-expiration-ms}")
     private int jwtExpMS;
 
-
+    /**
+     * When a user logs in, tthe generateJWTMethod will generate a new JSON Web Token (JWT) containing the header, payload and signiture.
+     * @param myUserDetails
+     * @return a JSON Web Token
+     */
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        return Jwts.builder()  //build the payload
+                .setSubject((myUserDetails.getUsername()))  //email address
+                .setIssuedAt(new Date())  // issued at current time
+                .setExpiration(new Date((new Date()).getTime() + jwtExpMS))  //current date + jwt expiration date
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)  //uses Sha256 algorithm with secret ket
+                .compact();
+    }
 }
