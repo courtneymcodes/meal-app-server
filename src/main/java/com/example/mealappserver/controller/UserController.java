365067862758f3beb17d7809077cd6739577e599
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-21 11:09:59 -0400

    add base request mapping

--- src/main/java/com/example/mealappserver/controller/UserController.java
+++ src/main/java/com/example/mealappserver/controller/UserController.java
@@ -2,9 +2,11 @@ package com.example.mealappserver.controller;
 
 import com.example.mealappserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "auth/users")
 public class UserController {
     private UserService userService;
     @Autowired
