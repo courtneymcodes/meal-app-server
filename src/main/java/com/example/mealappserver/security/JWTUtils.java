946372fb831e80bf2ca5bcd0bdf702698813f967
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-21 09:06:38 -0400

    implement method in JWTUtils to extract username from the jwt

--- src/main/java/com/example/mealappserver/security/JWTUtils.java
+++ src/main/java/com/example/mealappserver/security/JWTUtils.java
@@ -18,9 +18,9 @@ public class JWTUtils {
     private int jwtExpMS;
 
     /**
-     * When a user logs in, tthe generateJWTMethod will generate a new JSON Web Token (JWT) containing the header, payload and signiture.
+     * When a user logs in, the generateJWTMethod will generate a new JSON Web Token (JWT) containing the header, payload and signature.
      * @param myUserDetails
-     * @return a JSON Web Token
+     * @return a JSON Web Token with payload
      */
     public String generateJwtToken(MyUserDetails myUserDetails) {
         return Jwts.builder()  //build the payload
@@ -29,5 +29,14 @@ public class JWTUtils {
                 .setExpiration(new Date((new Date()).getTime() + jwtExpMS))  //current date + jwt expiration date
                 .signWith(SignatureAlgorithm.HS256, jwtSecret)  //uses Sha256 algorithm with secret ket
                 .compact();
+    }
+
+    /**
+     * Retrieves the username (user's email address) from JSON Web Token
+     * @param token The JWT to extract the username from
+     * @return the subject (username) extracted from the JWT
+     */
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).getBody().getSubject();
     }
 }
