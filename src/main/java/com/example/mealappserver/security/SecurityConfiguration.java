09096d30fb1f58d4d8b895000a2658543e2fa2e4
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 20:17:20 -0400

    create password encoder

--- src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
+++ src/main/java/com/example/mealappserver/security/SecurityConfiguration.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    /**
+     * Creates a password encoder used to encode and decode passwords
+     * @return BCryptPasswordEncoder instance
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
     /**
@@ -20,11 +30,11 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests().antMatchers("/auth/users","/auth/users/login/","/auth/users/register/").permitAll()  //allow access to this list of urls
-                .antMatchers("/h2-console/**").permitAll() // allow access to any url starting with /h2-console/
-                .anyRequest().authenticated()  //must authenticate any request
+                .antMatchers("/h2-console/**").permitAll() // allow access to any url starting with /h2-console/ for development
+                .anyRequest().authenticated()  //must authenticate any other requests
                 .and().sessionManagement()  //must manage user session
                 .and().csrf().disable() //disable Cross-Site Request Forgery protection
-                .headers().frameOptions().disable();  //for rendering h2 database
+                .headers().frameOptions().disable();  //for rendering h2 database during development
         return http.build();
     }
 }
