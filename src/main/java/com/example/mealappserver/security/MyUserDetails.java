77bb2aeea083093afc35cc8be24ad8bfe447cc9d
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-20 21:58:33 -0400

    implement MyUserDetails

---
+++ src/main/java/com/example/mealappserver/security/MyUserDetails.java
@@ -0,0 +1,56 @@
+package com.example.mealappserver.security;
+
+import com.example.mealappserver.model.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * MyUserDetails class implements Spring Securities UserDetails interface and contains information used for authentication and autherization such as authorities granted to user, whether the user's account has expired, whether the account locked and whether the user's credentials have expired.
+ */
+public class MyUserDetails implements UserDetails {
+
+    private final User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
