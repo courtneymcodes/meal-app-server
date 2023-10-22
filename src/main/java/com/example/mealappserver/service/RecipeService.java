06285e46614b685725c4c308e2a6faea361202d3
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-22 10:02:05 -0400

    create editRecipe method in RecipeService

--- src/main/java/com/example/mealappserver/service/RecipeService.java
+++ src/main/java/com/example/mealappserver/service/RecipeService.java
@@ -34,6 +34,21 @@ public class RecipeService {
         return recipeRepository.findAll();
     }
 
+    public Recipe editRecipe(Long recipeId, Recipe recipeObject) {
+        Recipe recipe = recipeRepository.findByIdAndUserId(recipeId, userService.getCurrentLoggedInUser().getId());
+        if (recipe != null) {
+            if (recipe.getName().equals(recipeObject.getName()) && recipe.getInstructions().equals(recipeObject.getInstructions())) {
+                throw new InformationExistsException("Recipe with that content already exists");
+            } else {
+                 recipe.setName(recipe.getName());
+                 recipe.setInstructions(recipe.getInstructions());
+                 return recipeRepository.save(recipe);
+            }
+        } else {
+            throw new InformationNotFoundException("Recipe with id " + recipeId + " not found");
+        }
+    }
+
     public Recipe getRecipe(Long recipeId) {
         Recipe recipe = recipeRepository.findByIdAndUserId(recipeId, userService.getCurrentLoggedInUser().getId());
         if (recipe != null){
