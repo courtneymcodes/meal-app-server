72d29d9225b9586c4e5bab291bd6e145f11e8d3f
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-25 12:28:35 -0400

    fix getAllRecipes in RecipeService to get only data for logged in user

--- src/main/java/com/example/mealappserver/repository/RecipeRepository.java
+++ src/main/java/com/example/mealappserver/repository/RecipeRepository.java
@@ -4,10 +4,14 @@ import com.example.mealappserver.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Recipe findByNameAndUserId(String recipeName, Long userId);
 
     Recipe findByIdAndUserId(Long recipeId, Long userId);
 
+
+    List<Recipe> findAllByUserId(Long recipeId);
 }
